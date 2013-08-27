0013bf4c3261f18eedb775578d26fe9dd0d8ab4c
ajest <pablojoelfu@hotmail.com>
2013-08-27 10:56:18 -0300

    Animations in OOP

--- js/battle-practice/main.js
+++ js/battle-practice/main.js
@@ -12,72 +12,89 @@ function init_battle_practice() {
         stage.update();
     }
     preload();
-    
+
     function handleComplete() {
-        function initilizeObjets(){
-            ken_prepared.init(queue.getResult("ken-still"));
+        function initilizeObjets() {
+            object.ken_prepared.init(queue.getResult("ken-still"));
+            animation.rig_left(object.ken_prepared.obj);
+        }
+
+        /**
+         * 
+         * Objects
+         */
+        
+        var object = new function() {
             
             /**
-             * Pasar las animaciones a POO
-             */
-            
-            createjs.Tween.get(ken_prepared.obj, {loop:true})
-                    .to({x:450}, 3000)
-                    .to({skewY:180})
-                    .to({x:50}, 3000);       
-        }
-        
-        /**
-         * 
-         * Objets
-         */
-        
-        /**
-         * 
-         * @type mixed
-         * Ken prepared
-         */
-        var ken_prepared = new function() {
-            this.obj = new createjs.Container();
-            this.bmpAnimation;
+            * 
+            * Ken prepared
+            */
+            this.ken_prepared = new function() {
+                this.obj = new createjs.Container();
+                this.bmpAnimation;
 
-            this.init = function(img) {
-                stage.addChild(ken_prepared.obj);
-                this.init_ken_prepared(img);
-                this.resize();
-            };
+                this.init = function(img) {
+                    stage.addChild(object.ken_prepared.obj);
+                    this.init_ken_prepared(img);
+                    this.resize();
+                };
 
-            this.init_ken_prepared = function(img) {
-                var spriteSheet = new createjs.SpriteSheet({
-                    images: [img],
-                    frames: {width: 70, height: 80, regX: 35, regY: 40},
-                    animations: {
-                        walk: [0, 3, "walk", 7]
-                    }
-                });
+                this.init_ken_prepared = function(img) {
+                    var spriteSheet = new createjs.SpriteSheet({
+                        images: [img],
+                        frames: {width: 70, height: 80, regX: 35, regY: 40},
+                        animations: {
+                            walk: [0, 3, "walk", 7]
+                        }
+                    });
 
-                createjs.SpriteSheetUtils.addFlippedFrames(spriteSheet, true, true, false);
-                ken_prepared.bmpAnimation = new createjs.BitmapAnimation(spriteSheet);
-                ken_prepared.bmpAnimation.regX = ken_prepared.bmpAnimation.spriteSheet.frameWidth / 2 | 0;
-                ken_prepared.bmpAnimation.regY = ken_prepared.bmpAnimation.spriteSheet.frameHeight / 2 | 0;
-                ken_prepared.bmpAnimation.gotoAndPlay("walk");
-                ken_prepared.bmpAnimation.shadow = new createjs.Shadow("#454", 0, 5, 4);
-                ken_prepared.bmpAnimation.name = "ken-still";
-                ken_prepared.bmpAnimation.direction = 90;
-                ken_prepared.bmpAnimation.vX = 0.3;
-                ken_prepared.bmpAnimation.currentFrame = 0;
-                ken_prepared.obj.addChild(ken_prepared.bmpAnimation);
-            };
+                    createjs.SpriteSheetUtils.addFlippedFrames(spriteSheet, true, true, false);
+                    object.ken_prepared.bmpAnimation = new createjs.BitmapAnimation(spriteSheet);
+                    object.ken_prepared.bmpAnimation.regX = object.ken_prepared.bmpAnimation.spriteSheet.frameWidth / 2 | 0;
+                    object.ken_prepared.bmpAnimation.regY = object.ken_prepared.bmpAnimation.spriteSheet.frameHeight / 2 | 0;
+                    object.ken_prepared.bmpAnimation.gotoAndPlay("walk");
+                    object.ken_prepared.bmpAnimation.shadow = new createjs.Shadow("#454", 0, 5, 4);
+                    object.ken_prepared.bmpAnimation.name = "ken-still";
+                    object.ken_prepared.bmpAnimation.direction = 90;
+                    object.ken_prepared.bmpAnimation.vX = 0.3;
+                    object.ken_prepared.bmpAnimation.currentFrame = 0;
+                    object.ken_prepared.obj.addChild(object.ken_prepared.bmpAnimation);
+                };
 
-            this.resize = function() {
-                ken_prepared.obj.x = 50;
-                ken_prepared.obj.y = 50;
+                this.resize = function() {
+                    object.ken_prepared.obj.x = 50;
+                    object.ken_prepared.obj.y = 50;
+                };
             };
         };
         
+        /**
+         *
+         * Animations 
+         */
+        
+        var animation = new function() {
+            
+            this.rig_left = function(obj) {
+                
+                createjs.Tween.get(obj, {loop: true})
+                    .to({x: 450}, 3000)
+                    .to({skewY: 180}, 150)
+                    .to({x: 50}, 3000)
+                    ;
+            };
+        
+        };
+        
+        /**
+         * 
+         * End of objects and animations
+         */
+
         initilizeObjets();
     }
-    
+
     /**
      * 
      * Preload all images of this canvas
@@ -87,7 +104,7 @@ function init_battle_practice() {
         queue = new createjs.LoadQueue(true);
         queue.loadFile({id: "ken-still", src: "img/ken-still.png"});
         queue.addEventListener("complete", handleComplete);
-        
+
         /**
          * Add images for preload
          */
@@ -96,6 +113,6 @@ function init_battle_practice() {
             {id: "ken-still2", src: "img/ken-still2.png"}
         ]);
     }
-    
-    
+
+
 }
